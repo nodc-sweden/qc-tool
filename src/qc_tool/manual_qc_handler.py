8068e80f94d42b86dcc26bbe6d15ad2180ec7a88
Jonatan Skogsfors <jonatan.skogsfors@smhi.se>
2024-08-29 09:13:48 +0200

    WIP 29/8

--- src/qc_tool/manual_qc_handler.py
+++ src/qc_tool/manual_qc_handler.py
@@ -1,12 +1,25 @@
-from bokeh.models import Div
+import jinja2
+from bokeh.models import Div, RadioButtonGroup, Column, Row
+from ocean_data_qc.fyskem.qc_flag import QcFlag, QC_FLAG_CSS_COLORS
 
 from qc_tool.protocols import Layoutable
 
+_flag_info_template = jinja2.Template("""
+<p>Points:</p>
+<ul start=0>
+    {% for value in values %}<li>
+        <font color="{{ qc_colors[value["manual_qc"]] }}">●</font> {{ value["depth"] }}: {{value["value"]}}
+    </li>{% endfor %}
+</ul>
+""")
 
 class ManualQcHandler(Layoutable):
     def __init__(self):
-        self._div = Div(text="HEJ")
+        values =
+        self._något_annat = Div(text=_flag_info_template.render(values=values, qc_colors=QC_FLAG_CSS_COLORS))
+        self._qc_buttons = RadioButtonGroup(labels=[flag.name.replace("_", " ").title() for flag in QcFlag], orientation="vertical")
 
     @property
     def layout(self):
-        return self._div
+        return Column(self._div, Row(self._qc_buttons))
+
